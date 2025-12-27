1813a52897bb9dd39a09f5c2e18588f56aacf2c5
WorkSpot-JP <asao.kamei@gmail.com>
2025-12-27 18:47:34 +0900

    Refactor: update namespace from `Mailer` to `Services` for consistency.

--- src/Services/AbstractHtmlMail.php
+++ src/Services/AbstractHtmlMail.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace WScore\Deca\Mailer;
+namespace WScore\Deca\Services;
 
 use WScore\Deca\Interfaces\MailInterface;
 use WScore\Deca\Interfaces\ViewInterface;
